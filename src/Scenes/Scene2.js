9621a2da765f0b8762973c44b7d7928c85d84f25
Aditya Yadav <ayadav@Ingenuity.local>
2024-10-10 05:36:26 -0700

    messing around with spritesheets

--- src/Scenes/Scene2.js
+++ src/Scenes/Scene2.js
@@ -7,40 +7,78 @@ export class Scene2 extends Phaser.Scene {
   }
 
   create() {
-    this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
+    this.background = this.add.tileSprite(
+      0,
+      0,
+      config.width,
+      config.height,
+      "background"
+    );
     this.background.setOrigin(0, 0);
-    this.ship = this.add.image(
+    this.ship1 = this.add.sprite(
       config.width / 2 - 50,
       config.height / 2,
       "ship"
     );
-    this.ship2 = this.add.image(config.width / 2, config.height / 2, "ship2");
-    this.ship3 = this.add.image(
+    this.ship2 = this.add.sprite(config.width / 2,
+      config.height / 2,
+      "ship2");
+
+    this.ship3 = this.add.sprite(
       config.width / 2 + 50,
       config.width / 2,
       "ship3"
     );
-    this.add.text(config.width/2 - 20, 20, "GO!!!", {
-      font: "25px Arial",
-      fill: "yellow",
+
+    this.anims.create({
+      key: "ship1_anim",
+      frames: this.anims.generateFrameNumbers("ship"),
+      frameRate: 20,
+      repeat: -1,
     });
+
+    this.anims.create({
+      key: "ship2_anim",
+      frames: this.anims.generateFrameNumbers("ship"),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "ship3_anim",
+      frames: this.anims.generateFrameNumbers("ship"),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "explosion",
+      frames: this.anims.generateFrameNumbers("explosion"),
+      frameRate: 20,
+      repeat: 0,
+      hideOnComplete: true
+    })
+
+    this.ship1.play("ship1_anim");
+    this.ship2.play("ship2_anim");
+    this.ship3.play("ship3_anim");
   }
-  update(){
+  update() {
     this.moveShip(this.ship, 3);
     this.moveShip(this.ship2, 5);
     this.moveShip(this.ship3, 7);
     this.background.tilePositionY -= 2;
   }
 
-  moveShip(ship, speed){
+  moveShip(ship, speed) {
     ship.y += speed;
-    if(ship.y > config.height){
+    if (ship.y > config.height) {
       ship.y = 0;
       this.resetShipPosi(ship);
     }
   }
 
-  resetShipPosi(ship){
+  resetShipPosi(ship) {
     ship.x = Phaser.Math.Between(0, config.width);
   }
 }
